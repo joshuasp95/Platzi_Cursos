c60a04aee04b7fc5356c1a6373897158e5cf6ab4
joshua <joshuasainz95@gmail.com>
2023-06-03 17:16:11 +0200

    Creada la BBDD con SQLite3 y Explorado Django CMD y Django Admin

--- Platzi_Django/premiosplatziapp/polls/models.py
+++ Platzi_Django/premiosplatziapp/polls/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_txt = models.CharField(max_length=150)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.question_txt
+
+    def was_published_recently(self):
+        # Booleano que comprueba si la fecha de publicacion es mayor o igual que (la fecha actual menos 1 dia)
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_txt = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_txt
--- Platzi_Django/premiosplatziapp/premiosplatziapp/settings.py
+++ Platzi_Django/premiosplatziapp/premiosplatziapp/settings.py
@@ -34,12 +34,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
+    "django.contrib.staticfiles"
 ]
 
 MIDDLEWARE = [
