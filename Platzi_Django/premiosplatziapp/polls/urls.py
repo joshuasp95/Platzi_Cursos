6b678d8807abe97d339c29a188837d23c4e46f92
joshua <joshuasainz95@gmail.com>
2023-06-04 01:46:47 +0200

    Creada la app siguiendo el patron MTV (Model, Template, View) pero con Function Based View

--- Platzi_Django/premiosplatziapp/polls/urls.py
+++ Platzi_Django/premiosplatziapp/polls/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
+
 from . import views
 
+app_name = "polls"
+
 urlpatterns = [
-    path("", views.index, name="index")
+    # /polls/
+    path("", views.index, name="index"),
+    # Example: /polls/5 -> accesso a detail del id de la pregunta
+    path("<int:question_id>/", views.detail, name="detail"),
+    # "" "" -> results
+    path("<int:question_id>/results/", views.results, name="results"),
+    # "" "" -> vote
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- Platzi_Django/premiosplatziapp/polls/views.py
+++ Platzi_Django/premiosplatziapp/polls/views.py
@@ -1,8 +1,43 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+from .models import Question, Choice
 
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list
+    })
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {
+        "question": question
+    })
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {
+        "question": question
+    })
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        # Devuelve ae valor del input con metodo POST del form
+        # Se accede por el name en caso del input
+        sel_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist) as e:
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "Error NO has elegido una respuesta"
+        })
+    else:
+        sel_choice.votes += 1
+        sel_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
