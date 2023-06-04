626899931cf6174a66ca10806009af6d40eea981
joshua <joshuasainz95@gmail.com>
2023-06-05 00:49:22 +0200

    Añadidos estilos y modificado el administrador de Django para poder crear questions con choices

--- Platzi_Django/premiosplatziapp/polls/admin.py
+++ Platzi_Django/premiosplatziapp/polls/admin.py
@@ -1,31 +1,40 @@
-from typing import Any
-from django import forms
-from django.contrib import admin
-from django.core.exceptions import ValidationError
-from .models import Question, Choice
+# Importando las bibliotecas necesarias
+from typing import Any  # typing es un módulo para sugerencias de tipo en Python
+from django import forms  # para manejar los formularios de Django
+from django.contrib import admin  # para manejar el sitio de administración de Django
+from django.core.exceptions import ValidationError  # para manejar errores de validación
+from .models import Question, Choice  # importando los modelos Question y Choice de la aplicación actual
 
+# Creando una clase para el formulario de una Question
+class QuestionForm(forms.ModelForm):  # heredamos de ModelForm para crear un formulario basado en el modelo Question
+    class Meta:  # Esta es una clase interna donde se definen algunas opciones de configuración
+        model = Question  # definimos el modelo en el que se basará este formulario
+        fields = '__all__'  # decidimos que todos los campos del modelo se incluyan en el formulario
 
-class QuestionForm(forms.ModelForm):
-    class Meta:
-        model = Question
-        fields = '__all__'
-
+    # Este método se llama cuando se limpia el formulario, es decir, cuando se verifica que todos los datos son válidos
     def clean(self):
-        super().clean()
+        super().clean()  # llamamos al método clean del padre para que realice la limpieza estándar
+        # si la instancia del formulario ya tiene una clave primaria (es decir, si ya está en la base de datos) y si no tiene choices asociados
         if self.instance.pk and not self.instance.choice_set.exists():
+            # lanzamos un error de validación
             raise ValidationError(
                 "La pregunta debe tener opciones asociadas antes de crearla")
 
+# Definiendo cómo se mostrará Choice en la interfaz de administración cuando se vea como parte de Question
+class ChoiceInLine(admin.StackedInline):  
+    model = Choice  # definimos qué modelo se mostrará
+    extra = 3  # decidimos que siempre habrá 3 campos extra de Choice listos para ser llenados en la interfaz de administración
 
-class ChoiceInLine(admin.TabularInline):
-    model = Choice
-    extra = 1
+# Definiendo cómo se mostrará Question en la interfaz de administración
+class QuestionAdmin(admin.ModelAdmin):  # heredamos de ModelAdmin para personalizar cómo se muestra Question
+    form = QuestionForm  # definimos que usaremos QuestionForm como el formulario para agregar y editar questions
+    inlines = [ChoiceInLine]  # definimos que Choice se mostrará en línea con Question
+    #Añadir filtros de busqueda y ordenamiento de la interfaz
+    list_display = ("question_txt", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_txt"]
 
-
-class QuestionAdmin(admin.ModelAdmin):
-    form = QuestionForm
-    inlines = [ChoiceInLine]
-
-
+# Registrando los modelos en la interfaz de administración con sus respectivas clases de administración personalizadas
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
+
